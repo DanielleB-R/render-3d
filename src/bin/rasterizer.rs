5ed0bc916bdcbc6475b7b6ba307cef1d7431fbf2
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-03-03 15:18:50 -0800

    Implement shaded triangles

--- src/bin/rasterizer.rs
+++ src/bin/rasterizer.rs
@@ -18,6 +18,33 @@ fn interpolate(i0: i32, d0: f64, i1: i32, d1: f64) -> Vec<f64> {
     values
 }
 
+#[derive(Debug, Clone, Copy)]
+struct ShadedVertex {
+    position: IVec2,
+    intensity: f64,
+}
+
+impl ShadedVertex {
+    fn new(x: i32, y: i32, h: f64) -> Self {
+        Self {
+            position: IVec2::new(x, y),
+            intensity: h,
+        }
+    }
+
+    fn x(&self) -> i32 {
+        self.position[0]
+    }
+
+    fn y(&self) -> i32 {
+        self.position[1]
+    }
+
+    fn h(&self) -> f64 {
+        self.intensity
+    }
+}
+
 trait LineCanvas {
     fn draw_line(&mut self, p0: IVec2, p1: IVec2, color: DVec3);
 }
@@ -49,6 +76,13 @@ impl LineCanvas for RgbImage {
 trait TriangleCanvas {
     fn draw_wireframe_triangle(&mut self, p0: IVec2, p1: IVec2, p2: IVec2, color: DVec3);
     fn draw_filled_triangle(&mut self, p0: IVec2, p1: IVec2, p2: IVec2, color: DVec3);
+    fn draw_shaded_triangle(
+        &mut self,
+        p0: ShadedVertex,
+        p1: ShadedVertex,
+        p2: ShadedVertex,
+        color: DVec3,
+    );
 }
 
 impl TriangleCanvas for RgbImage {
@@ -94,6 +128,59 @@ impl TriangleCanvas for RgbImage {
             }
         }
     }
+
+    fn draw_shaded_triangle(
+        &mut self,
+        mut p0: ShadedVertex,
+        mut p1: ShadedVertex,
+        mut p2: ShadedVertex,
+        color: DVec3,
+    ) {
+        if p1.y() < p0.y() {
+            mem::swap(&mut p1, &mut p0);
+        }
+        if p2.y() < p0.y() {
+            mem::swap(&mut p2, &mut p0);
+        }
+        if p2.y() < p1.y() {
+            mem::swap(&mut p2, &mut p1);
+        }
+
+        let mut x01 = interpolate(p0.y(), p0.x() as f64, p1.y(), p1.x() as f64);
+        let mut h01 = interpolate(p0.y(), p0.h(), p1.y(), p1.h());
+
+        let mut x12 = interpolate(p1.y(), p1.x() as f64, p2.y(), p2.x() as f64);
+        let mut h12 = interpolate(p1.y(), p1.h(), p2.y(), p2.h());
+
+        let x02 = interpolate(p0.y(), p0.x() as f64, p2.y(), p2.x() as f64);
+        let h02 = interpolate(p0.y(), p0.h(), p2.y(), p2.h());
+
+        x01.pop();
+        x01.append(&mut x12);
+
+        h01.pop();
+        h01.append(&mut h12);
+
+        let m = x01.len() / 2;
+        let (x_left, h_left, x_right, h_right) = if x02[m] < x01[m] {
+            (x02, h02, x01, h01)
+        } else {
+            (x01, h01, x02, h02)
+        };
+
+        for y in p0.y()..=p2.y() {
+            let i = (y - p0.y()) as usize;
+            let x_l = x_left[i].floor() as i32;
+            let x_r = x_right[i].floor() as i32;
+
+            let h_segment = interpolate(x_l, h_left[i], x_r, h_right[i]);
+
+            for x in x_l..=x_r {
+                let j = (x - x_l) as usize;
+                self.put_canvas_pixel(x, y, color * h_segment[j]);
+            }
+        }
+    }
 }
 
 fn main() {
@@ -106,10 +193,10 @@ fn main() {
         DVec3::new(255.0, 255.0, 255.0),
     );
 
-    buffer.draw_filled_triangle(
-        IVec2::new(-200, -250),
-        IVec2::new(200, 50),
-        IVec2::new(20, 250),
+    buffer.draw_shaded_triangle(
+        ShadedVertex::new(-200, -250, 0.0),
+        ShadedVertex::new(200, 50, 0.5),
+        ShadedVertex::new(20, 250, 1.0),
         DVec3::new(0.0, 255.0, 0.0),
     );
 
